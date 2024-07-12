83bdb8192e72897d4905925aad0841551c9b2430
usuario <usuario@host.docker.internal>
2024-07-12 09:27:16 -0500

    c3

--- Cuentas/src/main/java/com/pichincha/Cuentas/repository/ToClienteFromCuenta.java
+++ Cuentas/src/main/java/com/pichincha/Cuentas/repository/ToClienteFromCuenta.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.pichincha.Cuentas.configuration.FeignCuentaConfig;
 import com.pichincha.Cuentas.model.ClienteDTO;
 
-@FeignClient(name = "Cliente", url = "${cuentas_url}", configuration = FeignCuentaConfig.class)
+@FeignClient(name = "Cliente", url = "${cliente_url}", configuration = FeignCuentaConfig.class)
 public interface ToClienteFromCuenta {
 
 	@GetMapping("/clientes/{id}")
